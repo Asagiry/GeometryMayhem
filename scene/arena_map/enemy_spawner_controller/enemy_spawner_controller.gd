d0acee6f1a64175af35ff260535f44bbec922415
Voimax <voimaxis@mail.ru>
2025-11-08 23:56:32 +0300

    GM-124 Улучшена производительность спавна врагов и рефакторинг трейла движения

--- scene/arena_map/enemy_spawner_controller/enemy_spawner_controller.gd
+++ scene/arena_map/enemy_spawner_controller/enemy_spawner_controller.gd
@@ -7,89 +7,127 @@ extends Node
 
 var player: PlayerController
 var current_zone: ArenaZone
-var zone_current_enemy: Dictionary[ArenaZone,int]
+var zone_current_enemy: Dictionary[ArenaZone, int] = {}
+
+var _enemy_scene_cache: Dictionary = {}
 
 @onready var spawn_timer: Timer = %SpawnTimer
 
 
 func _ready() -> void:
+	if not enabled:
+		queue_free()
+		return
+		
 	arena_map.player_entered.connect(_on_player_entered)
 	arena_map.player_exited.connect(_on_player_exited)
-	if !enabled:
-		queue_free()
 
-func _on_player_entered(zone: ArenaZone):
-	if zone.get_name() != "StabilityZone":
-		current_zone = zone
-		print("Спавн начат в зоне ",zone.get_zone_name())
-		spawn_timer.start(zone.arena_stat_data.spawn_interval)
-	else:
-		print("Спавн окончен")
-		spawn_timer.stop()
 
-func _on_player_exited(_zone: ArenaZone):
+func _on_player_entered(zone: ArenaZone) -> void:
+	if zone.get_name() == "StabilityZone":
+		_stop_spawning()
+		return
+	
+	_start_spawning(zone)
+
+
+func _on_player_exited(_zone: ArenaZone) -> void:
 	pass
+
+
+func _start_spawning(zone: ArenaZone) -> void:
+	current_zone = zone
+	print("Спавн начат в зоне ", zone.get_zone_name())
+	spawn_timer.start(zone.arena_stat_data.spawn_interval)
+
+
+func _stop_spawning() -> void:
+	print("Спавн окончен")
+	spawn_timer.stop()
+	current_zone = null
+
 
 func _on_spawn_timer_timeout() -> void:
 	_spawn_enemy()
 
-func _spawn_enemy():
-	if current_zone:
-		if zone_current_enemy.get(current_zone, 0) < current_zone.arena_stat_data.max_enemies:
-			var spawn_point = current_zone.get_random_tile_point()
-			var enemy_scene: PackedScene = _get_random_enemy_scene(current_zone)
-			if (enemy_scene == null):
-				return
-			var enemy_instance = enemy_scene.instantiate() as EnemyController
-			enemy_instance.global_position = spawn_point
-			enemy_instance.stats.spawn_point = spawn_point
-			get_tree().get_first_node_in_group("back_layer").call_deferred("add_child",
-			enemy_instance)
-			zone_current_enemy[current_zone] = zone_current_enemy.get(current_zone, 0) + 1
-			enemy_instance.enemy_died.connect(_on_enemy_died.bind(current_zone))
 
-func _get_random_enemy_scene(zone: ArenaZone):
-	if (zone.get_zone_name()=="overload" || zone.get_zone_name()=="chaotic"):
+func _spawn_enemy() -> void:
+	if not current_zone:
 		return
-	var path = "res://scene/game_objects/enemies/"
-	var enemy_index = randi_range(1,3)#до трех
-	var enemy_type: String
-	match enemy_index:
-		1:
-			enemy_type = "melee_enemy"
-		2:
-			enemy_type = "range_enemy"
-		3:
-			enemy_type = "bomb_enemy"
+		
+	var current_enemy_count: int = zone_current_enemy.get(current_zone, 0)
+	if current_enemy_count >= current_zone.arena_stat_data.max_enemies:
+		return
+	
+	var spawn_point: Vector2 = current_zone.get_random_tile_point()
+	var enemy_scene: PackedScene = _get_random_enemy_scene(current_zone)
+	
+	if not enemy_scene:
+		return
+	
+	var enemy_instance: EnemyController = enemy_scene.instantiate() as EnemyController
+	enemy_instance.global_position = spawn_point
+	enemy_instance.stats.spawn_point = spawn_point
+	
+	var back_layer: Node = get_tree().get_first_node_in_group("back_layer")
+	if back_layer:
+		back_layer.call_deferred("add_child", enemy_instance)
+	
+	zone_current_enemy[current_zone] = current_enemy_count + 1
+	enemy_instance.enemy_died.connect(_on_enemy_died.bind(current_zone))
 
 
-	path += enemy_type + "/"
-	path += zone.get_zone_name()+"/"
+func _get_random_enemy_scene(zone: ArenaZone) -> PackedScene:
+	var zone_name: String = zone.get_zone_name()
+	
+	# Для специальных зон не спавним врагов
+	if zone_name in ["overload", "chaotic"]:
+		return null
+	
+	# Генерируем ключ для кэша
+	var cache_key: String = "%s_%d" % [zone_name, randi()]
+	if _enemy_scene_cache.has(cache_key):
+		return _enemy_scene_cache[cache_key]
+	
+	var enemy_types: Array = ["melee_enemy", "range_enemy", "bomb_enemy"]
+	var enemy_type: String = enemy_types[randi() % enemy_types.size()]
+	
+	var path: String = "res://scene/game_objects/enemies/%s/%s/" % [enemy_type, zone_name]
+	var enemy_variant_count: int = _get_enemy_variant_count(path)
+	
+	if enemy_variant_count <= 0:
+		return null
+	
+	var enemy_variant: int = randi_range(1, enemy_variant_count)
+	path += "%s_%d.tscn" % [enemy_type, enemy_variant]
+	
+	var scene: PackedScene = load(path) as PackedScene
+	if scene:
+		_enemy_scene_cache[cache_key] = scene
+	
+	return scene
 
-	var enemy_variant_count = _get_enemy_variant_count(path)
-	var enemy_variant = randi_range(1,enemy_variant_count)
 
-	path += enemy_type + "_" + str(enemy_variant)
-	path += ".tscn"
-	return load(path)
-
-
-func _get_enemy_variant_count(path: String):
-	var dir = DirAccess.open(path)
-	if dir:
-		dir.list_dir_begin()
-		var file_count = 0
-		var file_name = dir.get_next()
-		while file_name != "":
-			if not dir.current_is_dir():  # Только файлы, не папки
-				file_count += 1
-			file_name = dir.get_next()
-		dir.list_dir_end()
-		return file_count
-	return 0
+func _get_enemy_variant_count(path: String) -> int:
+	var dir: DirAccess = DirAccess.open(path)
+	if not dir:
+		return 0
+	
+	dir.list_dir_begin()
+	var file_count: int = 0
+	var file_name: String = dir.get_next()
+	
+	while file_name != "":
+		if not dir.current_is_dir() and file_name.ends_with(".tscn"):
+			file_count += 1
+		file_name = dir.get_next()
+	
+	dir.list_dir_end()
+	return file_count
 
 
-func _on_enemy_died(zone: ArenaZone):
-	zone_current_enemy[zone] -= 1
-	print("Текущее количество врагов в зоне ", zone.get_name(),
-	zone_current_enemy[zone])
+func _on_enemy_died(zone: ArenaZone) -> void:
+	if zone_current_enemy.has(zone):
+		zone_current_enemy[zone] = max(0, zone_current_enemy[zone] - 1)
+		print("Текущее количество врагов в зоне %s: %d" % [zone.get_name(), zone_current_enemy[zone]])
+
--- scene/game_objects/player/vfx/MovementTrailVFX/trail_line.gd
+++ scene/game_objects/player/vfx/MovementTrailVFX/trail_line.gd
@@ -7,11 +7,7 @@ var previous_position: Vector2
 var pivot_offset: Vector2
 var length: int
 
-func init(entity_node: Node2D,
-offset: Vector2,
-color: Color,
-trail_length: int,
-trail_width: int) -> void:
+func init(entity_node: Node2D, offset: Vector2, color: Color, trail_length: int, trail_width: int) -> void:
 	entity = entity_node
 	default_color = color
 	pivot_offset = offset
@@ -22,33 +18,34 @@ trail_width: int) -> void:
 
 func _physics_process(_delta: float) -> void:
 	if not is_enabled:
-		if points.size() > 0:
-			remove_point(0)
-			if points.size() == 0:
-				queue_free()
+		_fade_out()
 		return
 
 	if not entity:
 		return
 
-	var pos = entity.global_position
+	var current_position = entity.global_position
 	var angle = entity.global_rotation
-
 	var forward = Vector2.RIGHT.rotated(angle)
-
-	var right_vector = Vector2(forward.y, -forward.x).normalized()
+	var right = Vector2(forward.y, -forward.x).normalized()
 
 	# Учитываем отражение по X
 	if entity.scale.x < 0:
-		right_vector = -right_vector
+		right = -right
 
-	var offset_position = pos + pivot_offset.x * right_vector + pivot_offset.y * forward
-
-	add_point(offset_position)
-	previous_position = pos
+	var trail_position = current_position + pivot_offset.x * right + pivot_offset.y * forward
+	add_point(trail_position)
+	previous_position = current_position
 
 	if points.size() > length:
 		remove_point(0)
+
+
+func _fade_out() -> void:
+	if points.size() > 0:
+		remove_point(0)
+		if points.size() == 0:
+			queue_free()
 
 
 func destroy() -> void:
