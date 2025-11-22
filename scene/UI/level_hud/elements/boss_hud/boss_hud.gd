f75b7e0cde6d52f598dff4ab8f6061087ebe126a
Voimax <voimaxis@mail.ru>
2025-11-23 00:54:55 +0300

    GM-144 Добавлено + подключено BOSS_HP_BAR. local_to_scene добавлены к статам щупалец

--- scene/UI/level_hud/elements/boss_hud/boss_hud.gd
+++ scene/UI/level_hud/elements/boss_hud/boss_hud.gd
@@ -2,17 +2,19 @@ class_name BossInfoUI
 
 extends MarginContainer
 
-var hp_data: Dictionary = {}
+var is_timer_finished: bool = false
+var elapsed_after_finish: float = 0.0
+
 
 @onready var game_timer: Timer = %GameTimer
 @onready var timer_label: Label = %TimerLabel
-
 @onready var boss_hp_container: VBoxContainer = %BossHpContainer
-@onready var body_hp: Label = %BodyHP
-@onready var t_1hp: Label = %T1HP
-@onready var t_2hp: Label = %T2HP
-@onready var t_3hp: Label = %T3HP
-@onready var t_4hp: Label = %T4HP
+
+@onready var body_hp_bar: BossHPBarUI = %BodyHPBar
+@onready var tentacle_hp_bar_1: BossHPBarUI = %TentacleHPBar1
+@onready var tentacle_hp_bar_2: BossHPBarUI = %TentacleHPBar2
+@onready var tentacle_hp_bar_3: BossHPBarUI = %TentacleHPBar3
+@onready var tentacle_hp_bar_4: BossHPBarUI = %TentacleHPBar4
 
 func _ready() -> void:
 	Global.player_pulled.connect(_on_player_pulled)
@@ -20,17 +22,30 @@ func _ready() -> void:
 
 
 func _process(_delta: float) -> void:
-	if timer_label:
+	if not timer_label:
+		return
+
+	if not is_timer_finished:
 		timer_label.text = format_timer(game_timer.time_left)
+	else:
+		elapsed_after_finish += _delta
+		var negative_time = -elapsed_after_finish
+		timer_label.text = format_timer(negative_time)
 
 
-func format_timer(seconds: float):
-	var minutes = int(floor(seconds / 60))
-	var remaining_seconds = int(floor(seconds - (minutes * 60)))
-	return str(minutes) + ":" + "%02d" % (remaining_seconds)
+func format_timer(seconds: float) -> String:
+	var is_negative = seconds < 0
+	seconds = abs(seconds)
+
+	var minutes = int(seconds / 60)
+	var secs = int(fmod(seconds, 60))
+
+	var sign = "-" if is_negative else ""
+	return sign + str(minutes) + ":" + "%02d" % secs
 
 
 func _on_game_timer_timeout() -> void:
+	is_timer_finished = true
 	Global.game_timer_timeout.emit()
 
 
@@ -40,41 +55,36 @@ func _on_player_pulled():
 	boss_hp_container.visible = true
 	var boss = get_tree().get_first_node_in_group("boss") as BossController
 
-	#hp_data["T1HurtBox"] = {
-		#"current_health" = boss.stats.tentacle_max_hp,
-		#"max_health" = boss.stats.tentacle_max_hp,
-		#"label": t_1hp
-	#}
-	#hp_data["T2HurtBox"] = {
-		#"current_health" = boss.stats.tentacle_max_hp,
-		#"max_health" = boss.stats.tentacle_max_hp,
-		#"label" = t_2hp
-	#}
-	#hp_data["T3HurtBox"] = {
-		#"current_health" = boss.stats.tentacle_max_hp,
-		#"max_health" = boss.stats.tentacle_max_hp,
-		#"label" = t_3hp
-	#}
-	#hp_data["T4HurtBox"] = {
-		#"current_health" = boss.stats.tentacle_max_hp,
-		#"max_health" = boss.stats.tentacle_max_hp,
-		#"label" = t_4hp
-	#}
-	#hp_data["BodyHurtBox"] = {
-		#"current_health" = boss.stats.body_max_hp,
-		#"max_health" = boss.stats.body_max_hp,
-		#"label" = body_hp
-	#}
-	#for data in hp_data.values():
-		#data["label"].text = "hp = " + str(data["current_health"]) + "/" + str(data["max_health"])
-#
-	#boss.health_component.health_changed.connect(_on_health_decreased)
+	boss.health_component.health_decreased.connect(_on_body_hp_changed)
+	boss.health_component.health_increased.connect(_on_body_hp_changed)
 
-func _on_health_decreased(current_health: float, p_name: String):
-	if current_health<=0:
-		hp_data[p_name]["label"].queue_free()
-		hp_data.erase(p_name)
-		return
-	var data = hp_data[p_name]
-	data["current_health"] = current_health
-	data["label"].text = "hp = " + str(data["current_health"]) + "/" + str(data["max_health"])
+	for i in range(4):
+		boss.tentacle_controller.tentacles[i].\
+		health_component.\
+		health_increased.\
+		connect(_on_tentacle_hp_changed.bind(i))
+		boss.tentacle_controller.tentacles[i].\
+		health_component.\
+		health_decreased.\
+		connect(_on_tentacle_hp_changed.bind(i))
+
+
+func _on_body_hp_changed(current_health: float,max_health: float):
+	body_hp_bar.max_value = max_health
+	body_hp_bar.value = current_health
+
+
+func _on_tentacle_hp_changed(current_health: float, max_health: float, id: int):
+	match id:
+		0:
+			tentacle_hp_bar_1.max_value = max_health
+			tentacle_hp_bar_1.value = current_health
+		1:
+			tentacle_hp_bar_2.max_value = max_health
+			tentacle_hp_bar_2.value = current_health
+		2:
+			tentacle_hp_bar_3.max_value = max_health
+			tentacle_hp_bar_3.value = current_health
+		3:
+			tentacle_hp_bar_4.max_value = max_health
+			tentacle_hp_bar_4.value = current_health
